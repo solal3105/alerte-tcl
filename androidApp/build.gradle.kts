cf5481f986d958b954a6f8998e1be9296dc159a6
Solal Gendrin <solal.gendrin@example.com>
2026-05-22 09:54:00 +0200

    chore: bump version to 1.2 (build 2)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.alertetcl.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
     }
 
     buildFeatures {
